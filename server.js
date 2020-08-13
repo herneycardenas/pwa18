69e6145a8e79b406ca79ddc7a4ba3e9517b16b26
Herney Cardenas <i.herneyc@gmail.com>
2020-08-13 02:13:53 -0700

    catche storage working

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 3005;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
